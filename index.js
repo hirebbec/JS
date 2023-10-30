7828434b5b6cd8601f6eae5593665470bc86be1e
Ruzel <reek867@gmial.com>
2023-10-30 19:23:39 +0300

    ez

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 let countEl = document.getElementById("count-el")
 let count = Number(countEl.textContent)
 
+let prevEl = document.getElementById("prev-p")
+
 function increment() {
     count = count + 1
-    countEl.innerText = count
+    countEl.textContent = count
 }
 
+function save() {
+    prevEl.textContent += " - " + countEl.innerText
+    countEl.textContent = 0
+    count = 0
+}
 
