bce7789809feea199a825ad3cbe60f5ea3743955
Ruzel <reek867@gmial.com>
2023-11-06 15:13:32 +0300

    add task 3

---
+++ task3/index.js
@@ -0,0 +1,10 @@
+// Refactor the code so that it uses .addEventListener()
+// when you click the SAVE INPUT button
+
+let inputBtnEl = document.getElementById("input-btn")
+inputBtnEl.addEventListener("click", function() {
+    console.log("button clicked!")
+})
+
+
+
