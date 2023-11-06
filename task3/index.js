0bfbd091455742c60ff9089401ea7f2b586c8038
Ruzel <reek867@gmial.com>
2023-11-06 16:14:22 +0300

    add renderLeads func

--- task3/index.js
+++ task3/index.js
@@ -1,18 +1,19 @@
 let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-const unEl = document.getElementById("ul-el")
-// Render the leads in the unordered list using ulEl.textContent
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    renderLeads()
 })
 
-
-for (let i = 0; i < myLeads.length; i++) {
-    unEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+// 1. Wrap the code below in a renderLeads() function
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>"
+    }
+    ulEl.innerHTML = listItems
 }
-
-
 
