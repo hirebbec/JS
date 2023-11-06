ede95374074a46d0e1a6b6ca90213198599aea29
Ruzel <reek867@gmial.com>
2023-11-06 16:33:16 +0300

    clear input after push

--- task3/index.js
+++ task3/index.js
@@ -1,19 +1,19 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
+    // Clear out the input field
+    inputEl.value = ""
     renderLeads()
 })
 
-// 1. Wrap the code below in a renderLeads() function
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
         listItems += "<li>" + myLeads[i] + "</li>"
     }
-    ulEl.innerHTML = listItems
+    ulEl.innerHTML = listItems  
 }
-
